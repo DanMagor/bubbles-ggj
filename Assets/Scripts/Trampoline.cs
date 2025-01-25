a05f18d9cdbc181bddc7e9c9ec899bb63111c656
Bulat Igebaev <pingvin13lol@gmail.com>
2025-01-25 19:47:07 +0300

    Edited bouncer and added spikes

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -4,15 +4,19 @@ public class Trampoline : MonoBehaviour
 {
     [Header("Trampoline Settings")]
     [SerializeField] private float _launchForce = 10f;
+    [SerializeField] private float _stunDuration = 0.5f;
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Player")
         {
             var rb = col.rigidbody;
+            var stun = rb.gameObject.GetComponent<PlayerController>();
+            stun.StunPlayer(_stunDuration);
             rb.velocity = Vector3.zero;
             Vector3 launchDirection = transform.up;
             rb.AddForce(launchDirection * _launchForce, ForceMode.Impulse);
         }
+        
     }
 }
